ae3b2a2e46ae3f47bc658ac00953c96f00e90e21
Florian-H-Lab <f.heyl@gmx.de>
2019-06-12 15:02:54 +0200

    Changes to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N PanPeaker
 #$ -cwd
-#$ -pe smp 10
+#$ -pe smp 1
 #$ -l h_vmem=4G
 #$ -o /scratch/bi03/heylf/Snakemake_Cluster_Logs/
 #$ -e /scratch/bi03/heylf/Snakemake_Cluster_Logs/
@@ -14,10 +14,10 @@ python PanPeaker.py \
 -b /scratch/bi03/heylf/PanPeaker/test/RBFOX2_rep1.bai /scratch/bi03/heylf/PanPeaker/test/RBFOX2_rep2.bai \
 -c /scratch/bi03/heylf/PanPeaker/test/Input.bam \
 -k /scratch/bi03/heylf/PanPeaker/test/Input.bai \
--nt 10 \
+-nt 1 \
 -g ../genomes/hg19/GRCh37.p13.genome.fa -o /scratch/bi03/heylf/PanPeaker \
 --chr_sizes ../genomes/hg19/hg19_chr_sizes.txt \
---seed 123
+--seed 123 --refinement
 source deactivate panpeaker
 
 
